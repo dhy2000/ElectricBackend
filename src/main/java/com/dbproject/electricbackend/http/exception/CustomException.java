0dc328c6cdf9e0e153fd0c2eb3912b2bf8ad2dbe
donghanyuan <dhy_120@163.com>
2021-09-17 23:45:13 +0800

    调整统一自定义异常类

--- src/main/java/com/dbproject/electricbackend/http/exception/CustomException.java
+++ src/main/java/com/dbproject/electricbackend/http/exception/CustomException.java
@@ -1,20 +1,15 @@
 package com.dbproject.electricbackend.http.exception;
 
+import lombok.Getter;
+
 public class CustomException extends Exception {
+    @Getter
     private final int code;
+    @Getter
     private final String message;
 
     public CustomException(int code, String message) {
         this.code = code;
         this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
     }
 }
